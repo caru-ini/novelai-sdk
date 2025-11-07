2f4659c32c0e6c1e1ee749505b39c81e39daf05e
caru-ini <caru17122@gmail.com>
2025-11-08 00:07:47 +0900

    feat: implement metadata eraser utility

--- src/novelai/utils/metadata.py
+++ src/novelai/utils/metadata.py
@@ -1,5 +1,6 @@
-from typing import Any
+from typing import Any, Literal
 
+from PIL.Image import Image
 from pydantic import BaseModel, ConfigDict
 
 from novelai._utils.image import ImageInput, to_pil_image
@@ -26,11 +27,36 @@ def extract_metadata(image: ImageInput) -> ImageMeta:
     image = to_pil_image(image)
 
     png_info = getattr(image, "info", None)
-    alpha_info = extract_image_metadata(image=image, get_fec=False)
-
+    alpha_info = None
+    if image.mode == "RGBA":
+        # skip if alpha is not present
+        alpha_info = extract_image_metadata(image=image, get_fec=False)
     if not isinstance(alpha_info, dict):
         alpha_info = None
 
     meta = ImageMeta(png_info=png_info, alpha_info=alpha_info)
 
     return meta
+
+
+def erase_metadata(
+    image: ImageInput, target: Literal["alpha", "png_info", "both"] = "both"
+) -> Image:
+    """Erase metadata from an image.
+
+    Args:
+        image (ImageInput): The image to erase metadata from.
+        target Literal["alpha","png_info", "both"]: Data target to erase.
+
+    Returns
+        Image: PIL Image object.
+    """
+    new_image = to_pil_image(image)
+    if target in ["both", "alpha"]:
+        print("erase Alpha")
+        new_image = new_image.convert("RGB")
+    if target in ["both", "png_info"]:
+        print("erase info")
+        new_image.info = {}
+
+    return new_image
