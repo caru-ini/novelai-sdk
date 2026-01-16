69426ef1ba701b57c62fbea3d20b79ed08b88f7c
caru-ini <caru17122@gmail.com>
2026-01-16 23:08:53 +0900

    docs(i18n): add simplified chinese(zh-hans) translation

--- website/docusaurus.config.ts
+++ website/docusaurus.config.ts
@@ -1,5 +1,5 @@
-import {themes as prismThemes} from 'prism-react-renderer';
-import type {Config} from '@docusaurus/types';
+import { themes as prismThemes } from 'prism-react-renderer';
+import type { Config } from '@docusaurus/types';
 import type * as Preset from '@docusaurus/preset-classic';
 
 // This runs in Node.js - Don't use client-side code here (browser APIs, JSX...)
@@ -33,7 +33,7 @@ const config: Config = {
   // may want to replace "en" with "zh-Hans".
   i18n: {
     defaultLocale: 'en',
-    locales: ['ja', 'en'],
+    locales: ['ja', 'en', 'zh-Hans'],
   },
 
   presets: [
